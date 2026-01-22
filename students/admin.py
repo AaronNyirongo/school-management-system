ebe1372a09584c81ead6841370246d5d22dceb30
Aaron Nyirongo <aaronnyirongo28@gmail.com>
2026-01-22 00:53:05 -0800

    Add students app and Student model

---
+++ students/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Student
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("enrollment_number", "first_name", "last_name", "guardian_phone")
+    search_fields = ("enrollment_number", "first_name", "last_name")
---
+++ students/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class Student(models.Model):
+    GENDER_CHOICES = [
+        ("M", "Male"),
+        ("F", "Female"),
+        ("O", "Other"),
+    ]
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    enrollment_number = models.CharField(max_length=30, unique=True)
+
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+
+    guardian_phone = models.CharField(max_length=20, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.enrollment_number})"
