6f82505e5694babf2f0d1355225462cc54471ecf
Aaron Nyirongo <aaronnyirongo28@gmail.com>
2026-01-26 00:48:07 -0800

    Add subjects module

---
+++ subjects/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    code = models.CharField(max_length=20, unique=True)
+    is_active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.code})"
